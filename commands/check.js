8a7f93ad03dc573157dc52a1372425cfe96c8c1d
crypt096 <acvjetan@gmail.com>
2020-04-25 21:34:28 +0200

    Initial commit.

---
+++ commands/check.js
@@ -0,0 +1,23 @@
+// API
+const API_KEY = "fb164bab4546a4336c12792fc6b22e27";
+const KeyManager = require("../lib/KeyManager");
+const CryptoAPI = require("../lib/CryptoAPI");
+
+const check = {
+  async price(cmd) {
+    try {
+      keyManager = new KeyManager();
+      const key = keyManager.getKey();
+
+      const api = new CryptoAPI(key);
+
+      const priceOutputData = await api.getPriceData(cmd.coin, cmd.cur);
+
+      console.log(priceOutputData);
+    } catch (error) {
+      console.error(error.message.red);
+    }
+  },
+};
+
+module.exports = check;
